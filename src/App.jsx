c3a24d08fd29b83f854f1cc1b51a719e48eed715
endfastpls712 <phamhieu7102@gmail.com>
2025-02-28 08:48:16 +0700

    add 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,17 @@ import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import Layout from './Layout'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000')
+      .then(response => setMessage(response.data.message))
+      .catch(error => console.error(error));
+  }, []);
 
   return (
     <Routes>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const RegisterPage = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  function registerUser() {
+    axios.get('http://localhost:5000/');
+  }
   return (
     <div className='mt-4 grow flex items-center justify-around' >
     <div className='mb-64'>
      <h1 className='text-4xl text-center mb-4'>Register</h1>
-     <form className='max-w-md mx-auto '>
-        <input className='w-full border my-2 py-2 px-3 rounded-lg' type="text" placeholder='username'/>
-       <input className='w-full border my-2 py-2 px-3 rounded-lg' type="email" placeholder='your@email.com' />
-       <input className='w-full border my-2 py-2 px-3 rounded-lg'type="password" placeholder='password' />
+     <form className='max-w-md mx-auto ' onSubmit={''}>
+        <input className='w-full border my-2 py-2 px-3 rounded-lg' type="text" placeholder='username' 
+        value={username} 
+        onChange={e => setUsername(e.target.value)}/>
+       <input className='w-full border my-2 py-2 px-3 rounded-lg' type="email" placeholder='your@email.com' 
+       value={email} 
+       onChange={e => setEmail(e.target.value)}/>
+       <input className='w-full border my-2 py-2 px-3 rounded-lg'type="password" placeholder='password' 
+       value={password} 
+       onChange={e => setPassword(e.target.value)}/>
        <button className='bg-gray-300 primary p-2 w-full text-white rounded-full'>Login</button>
        <div className='text-center py-2 text-gray-500'>
         Already a member? <Link className='underline text-bn' to={'/login'}>Login</Link>
